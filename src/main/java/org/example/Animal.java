28578a1b0012ff13fca634138ad4946acbca7b10
Mirzabek Safaraliyev <safaraliyevmirzabek2006@gmail.com>
2026-02-02 23:30:39 +0500

    subclassandsuperclass

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,11 @@
+package org.example;
+//Superclass
+//Bu yerda Animal -superclass
+//name-field
+//eat()- metod
+public class Animal {
+String name;
+void eat(){
+    System.out.println("Animal ovqat yeydi");
+}
+}
---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,16 @@
+package org.example;
+//Sublcass superclassdan meros olish
+// Dog -subclass extends Animal -Animal classda meros olayapti
+// Dog class eat metodini bu yerda avtomatik oladi
+public class Dog extends Animal {
+
+//    void bark(){
+//        System.out.println("It vovullaydi");
+//    }
+
+//    @Override
+//    void eat() {
+//        System.out.println("Dog eat");
+//    }
+
+}
